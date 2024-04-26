fb86cd4b2cc703f1b88797e77797a78f1e75b6f8
schleising <stephen@schleising.net>
2024-04-26 14:02:44 +0100

    Add signal handling and error handling in converter.go

--- converter.go
+++ converter.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/schleising/go-ffmpeg"
 )
 
 func main() {
+	// Create a context with a cancel function
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	// Defer the cancel function
+	defer cancelFunc()
+
+	// Create a signal channel
+	signalChannel := make(chan os.Signal, 1)
+
+	// Notify the signal channel of any interrupt signals
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	// Create a goroutine to listen for signals
+	go func() {
+		// Wait for a signal
+		<-signalChannel
+
+		// Cancel the context
+		cancelFunc()
+	}()
+
+	// Create a new Ffmpeg instance
 	ffmpeg, err := go_ffmpeg.NewFfmpeg(
+		ctx,
 		"/Users/steve/Downloads/TestInput.mp4",
 		"/Users/steve/Downloads/Converted/TestOutput.mp4",
 		[]string{
@@ -22,14 +49,27 @@ func main() {
 	}
 
 	go func() {
-		for progress := range ffmpeg.Progress {
-			fmt.Println(progress)
+		for {
+			select {
+			case progress, ok := <-ffmpeg.Progress:
+				if !ok {
+					fmt.Println("Progress channel closed")
+					return
+				}
+				fmt.Println(progress)
+			case err, ok := <-ffmpeg.Error:
+				if !ok {
+					fmt.Println("Error channel closed")
+					return
+				}
+				fmt.Println(err)
+			}
 		}
 	}()
 
 	err = ffmpeg.Start()
 
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go-converter
 go 1.22.2
 
 require github.com/schleising/go-ffmpeg v0.0.1
+
+replace github.com/schleising/go-ffmpeg => ../go-ffmpeg
