614993ec7dc34d58cb79b31015101a03c73062b3
schleising <stephen@schleising.net>
2024-04-30 22:22:28 +0100

    Update to build the go application in a container

---
+++ converter/src/converter.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/schleising/go-ffmpeg"
+)
+
+// Structure to hold a converter job instance
+type Converter struct {
+	// File to convert
+	inputFile string
+
+	// Channel to send progress information
+	progressChannel chan go_ffmpeg.Progress
+
+	// Context for the converter job
+	ctx context.Context
+
+	// Cancel function for the converter job
+	cancelFunc context.CancelFunc
+}
+
+// Create a new converter instance
+func NewConverter(inputFile string, progressChannel chan go_ffmpeg.Progress, ctx context.Context, cancelFunc context.CancelFunc) *Converter {
+	// Create a new converter instance
+	converter := Converter{
+		inputFile:       inputFile,
+		progressChannel: progressChannel,
+		ctx:             ctx,
+		cancelFunc:      cancelFunc,
+	}
+
+	// Return the converter instance
+	return &converter
+}
+
+// Convert a file using ffmpeg
+func (converter *Converter) convert() error {
+	// Wait for the file to complete copying before starting the conversion
+	var fileSize int64 = -1
+	CopyLoop:
+	for {
+		select {
+		case <-time.After(5 * time.Minute):
+			// Return an error if the file copy times out
+			return fmt.Errorf("file copy timed out")
+		default:
+			// Get the file info
+			fileInfo, err := os.Stat(converter.inputFile)
+			if err != nil {
+				return err
+			}
+
+			// Check if the file size has changed
+			if fileInfo.Size() != fileSize {
+				if fileInfo.Size() != 0 {
+					// Update the file size
+					fileSize = fileInfo.Size()
+				}
+
+				// Sleep for 1 second
+				time.Sleep(1 * time.Second)
+			} else {
+				// Break out of the loop as the file has finished copying
+				break CopyLoop
+			}
+		}
+	}
+
+	// Create a new Ffmpeg instance
+	ffmpeg, err := go_ffmpeg.NewFfmpeg(
+		converter.ctx,
+		converter.inputFile,
+		[]string{
+			"-c:v", "libx264",
+			"-c:a", "copy",
+			"-c:s", "copy",
+		},
+	)
+
+	// Check for errors
+	if err != nil {
+		return err
+	}
+
+	// Create a goroutine to listen for progress and errors
+	go func() {
+		for {
+			select {
+			// Listen for progress, requests, and errors
+			case progress, ok := <-ffmpeg.Progress:
+				if !ok {
+					return
+				}
+				converter.progressChannel <- progress
+			case err, ok := <-ffmpeg.Error:
+				if !ok {
+					return
+				}
+				fmt.Println("Parsing Error:", err)
+			}
+		}
+	}()
+
+	// Start the ffmpeg process
+	err = ffmpeg.Start()
+
+	// Check for errors
+	if err != nil {
+		return err
+	}
+
+	// Wait for the ffmpeg process to finish
+	<-ffmpeg.Done
+
+	// Return nil
+	return nil
+}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module go-converter
-
-go 1.22.2
-
-require github.com/schleising/go-ffmpeg v0.0.4
