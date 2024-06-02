34fef2d4231d699cd02d9ce634aaf16924e21f22
schleising <stephen@schleising.net>
2024-06-02 15:55:20 +0100

    Clean up application termination

--- converter/src/main.go
+++ converter/src/main.go
@@ -25,6 +25,9 @@ func main() {
 	// Create a progress channel
 	progressChannel := make(chan go_ffmpeg.Progress)
 
+	// Create a channel to indicate that the application is being terminated
+	terminatedChannel := make(chan struct{})
+
 	// Create a goroutine to convert files
 	go func() {
 		// Loop forever
@@ -33,13 +36,13 @@ func main() {
 			select {
 			case <-job.ctx.Done():
 				// Print a message to indicate that the conversion has been cancelled
-				fmt.Println("Conversion cancelled: ", job.inputFile)
+				fmt.Println("Conversion cancelled:", job.inputFile)
 
 				// Send an empty progress struct to indicate that the conversion is complete
 				progressChannel <- go_ffmpeg.Progress{}
 			default:
 				// Print a message to indicate that the conversion has started
-				fmt.Println("Converting file: ", job.inputFile)
+				fmt.Println("Converting file:", job.inputFile)
 
 				// Convert the file
 				err := job.convert()
@@ -47,11 +50,10 @@ func main() {
 				// Check for errors
 				if err != nil {
 					// Print an error message
-					fmt.Println("Error converting file: ", job.inputFile)
-					fmt.Println(err)
+					fmt.Println("Error converting file:", job.inputFile, err)
 				} else {
 					// Print a message to indicate that the conversion is complete
-					fmt.Println("Conversion complete: ", job.inputFile)
+					fmt.Println("Conversion complete:", job.inputFile)
 
 					// Call the cancel function
 					job.cancelFunc()
@@ -64,6 +66,9 @@ func main() {
 
 		// Close the progress channel
 		close(progressChannel)
+
+		// Send a signal to indicate that the channel is closed
+		terminatedChannel <- struct{}{}
 	}()
 
 	// Create an empty map for Converter jobs
@@ -74,20 +79,6 @@ func main() {
 
 	// Notify the channel on interrupt or terminate
 	signal.Notify(notifyChannel, syscall.SIGINT, syscall.SIGTERM)
-
-	// Create a goroutine to listen for notifications
-	go func() {
-		// Wait for a notification
-		<-notifyChannel
-
-		// Cancel all the jobs
-		for _, job := range jobs {
-			job.cancelFunc()
-		}
-
-		// Close the converter job channel
-		close(converterJobChannel)
-	}()
 
 	// Create a new server
 	server := NewServer()
@@ -160,6 +151,19 @@ func main() {
 		case <-server.requestChannel:
 			// Send the progress information
 			server.progressChannel <- progress
+		case <-notifyChannel:
+			// Got a signal to close the server
+			// Cancel all the jobs
+			for _, job := range jobs {
+				fmt.Println("Cancelling job:", job.inputFile)
+				job.cancelFunc()
+			}
+
+			// Close the converter job channel
+			close(converterJobChannel)
+
+			// Set the closing variable to true
+			closing = true
 		default:
 			// Sleep for 100 milliseconds
 			time.Sleep(100 * time.Millisecond)
@@ -178,12 +182,16 @@ func main() {
 		}
 	}
 
+	// Wait for the signal to close the channel
+	<-terminatedChannel
+
 	// Stop the server
 	err := server.Stop()
 
 	// Check for errors
 	if err != nil {
-		fmt.Println("Error stopping server")
-		fmt.Println(err)
+		fmt.Println("Error stopping server:", err)
+	} else {
+		fmt.Println("Application terminated successfully")
 	}
 }
